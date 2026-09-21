06502d8ddfe246773b421ce21f33fafc12b10e32
xinyadai <xinya.dai@my.jcu.edu.au>
2026-09-21 15:22:43 +0800

    add a new python

--- week1 classpra.py
+++ week1 classpra.py
@@ -2,19 +2,19 @@
 get gifts
 get students
 average_of_number= gifts // students
-residue_of_number = gifts - gifts // students
+residue_of_number =gifts % students
 display average_of_number
 display residue_of_number
 """
 # gifts = int(input("Enter the number of gifts"))
 # students = int(input("Enter the number of students"))
 # each_students_number = gifts // students
-# residue_of_number = gifts - gifts // students
+# residue_of_number = gifts % students
 # print(each_students_number)
 # print(residue_of_number)
 
 """
-1.if,else
+1.if,else,else
 2.if
 3.if,elif,else
 4.if,else
@@ -31,13 +31,13 @@ else:
     
 display final_price
 """
-# item_price = int(input("Enter item price: "))
+# item_price = float(input("Enter item price: $"))
 # has_GST = input("Do you have GST? (y/n): ")
 # if has_GST == "y":
 #     final_price = item_price * 1.1
 # else:
 #     final_price = item_price
-# print(final_price)
+# print(f"${final_price:.2f}")
 
 """
 1.for
@@ -47,21 +47,58 @@ display final_price
 5.while
 6.for
 """
+# number = int(input("Enter a number: "))
+# count = 1
+# while count <= number:
+#     print(count)
+#     count += 1
+#
+# number = int(input("Enter a number: "))
+# for i in range(1, number+1):
+#     print(i)
+
 
 # SECRET_NAME = 5
-# user_number = int(input("Enter a number between 1 and 100: "))
-# while user_number != SECRET_NAME:
-#     print("Sorry,not correct try again:")
-#     user_number = int(input("Enter a number between 1 and 100: "))
-# print("Correct!You are so intelligent.")
+# use_number = int(input("Enter a number to 1-10:"))
+# while use_number != SECRET_NAME:
+#     print("It not correct please try again")
+#     use_number = int(input("Guess again:"))
+#
+# print("Correct!You guess the secret name.")
 
 # user_name = input("Enter your name: ")
-# while user_name == " ":
-#     print("Name can't be blank,try again: ")
+# while user_name == "":
+#     print("Name cannot be blank")
 #     user_name = input("Enter your name: ")
-# salary = float(input("Enter your salary: "))
-# while salary < 0:
-#     print("Salary can't be below zero, try again: ")
-#     salary = float(input("Enter your salary: "))
+#
+# user_salary = float(input("Enter your salary: "))
+# while user_salary < 0:
+#     print("Salary cannot be below zero")
+#     user_salary = float(input("Enter your salary: "))
+#
 # print("Name:",user_name.upper())
-# print("Salary: ${:,.2f}",format(salary))
+# print(f"Salary: ${user_salary:,.2f}")
+
+user_number = int(input("Have many age?: "))
+
+total = 0
+for i in range(user_number):
+    age = int(input("Enter your Age: "))
+    total += age
+
+average = total / user_number
+print(total)
+print(average)
+
+age = int(input("Enter your Age: "))
+total = 0
+count = 0
+
+while age != -1:
+    total += age
+    count += 1
+    age = int(input("Enter your Age: "))
+
+average = total / count
+print(total)
+print(count)
--- week2 classprac.py
+++ week2 classprac.py
@@ -1,75 +1,57 @@
 # import random
-# #form random import
-# from random import randint
-#
-# random_number = randint(10, 100)
-#
-# print(random_number)
 
-AGE = 9
-
-def main():
-#     print_line("#",5)
+# low_number = int(input("Enter a low number: "))
+# high_number = int(input("Enter a high number: "))
+# while high_number <= low_number:
+#     print("high number must be grater than low number")
+#     high_number = int(input("Enter a high number: "))
 #
-#     age = calculate_sum(25,100)
-#     print(age)
-#
-#     print(is_even(60))
-#
-#     movie_hour,movie_minutes = convert_movie_duration(289)
-#     print(movie_hour,movie_minutes)
-#
-#     date_of_birth = (13,4,1976)
-#     print(format_date(*date_of_birth))
-#
-# def format_date(day,month,year):
-#     return f"{day}-{month}-{year}"
-#
-# def print_line(char="$",number=20):
-#     print(char * number)
-#
-# def calculate_sum(age,number):
-#     return age + number
-#
+# n = random.randint(low_number, high_number)
+# for i in range(n):
+#     print(":)",end="")
 
-    assert is_even(11)
+"""
+1.What are the three benefits of defining your own functions?
+easy to debug,have control dool,more define and change function.
 
-def is_even(number):
-    return number % 2 == 0
-#
-# def convert_movie_duration(movie_duration):
-#     movie_hour = movie_duration // 60
-#     movie_minutes = movie_duration % 60
-#     return movie_hour, movie_minutes
+2.According to the differences in the return values of functions, into which two major categories can functions be classified?
+Value-returning function（有返回值的函数）and
+Non-value-returning function / Void function（没有返回值的函数）
+The core lies in whether a result has been sent back through a return.
+"""
 
-    for i in range(0,101):
-        print(f"{i} - {score_grade(i)}")
+# def is_even(number):
+#     if number % 2 == 0:
+#         return True
+#     else:
+#         return False
 
-def score_grade(score):
-    if score >= 85:
-        grade = "HD"
-    elif score >= 75:
-        grade = "D"
-    elif score >= 65:
-        grade = "C"
-    else:
-        grade = "F"
-    return grade
-main()
+"""
+1.determine_grade
+2.convert_usd_to_aud
+3.print_report
+4.calculate_average
+5.is_even
+6.get_valid_number
+动作 + 对象 
+Be clear and meaningful
+example for 3:
+is_positive → 是正数吗？
+is_valid    → 是有效的吗？
+is_empty    → 是空的吗？
+is_adult    → 是成年人吗？
+"""
 
-import random
-def main():
-    low_number = int(input("Enter a number: "))
-    high_number = int(input("Enter a number: "))
-    while high_number <= low_number:
-        print("high number must be greater than low number")
-        high_number = int(input("Enter a number: "))
+"""
+function must include doc string
+On function
+Parameter（参数/形式参数）= 定义函数时写的“变量名”
+Argument（实际参数）= 调用函数时真正传进去的“值”
 
+def add(a, b):
+    return a + b
 
-def read_file():
-    in_file = open("read.txt")
-    content = in_file.read()
-    in_file.close()
-    return content
-
-print(read_file())
+result = add(10, 20)
+a,b = parameter
+10,20 = argument
+"""
